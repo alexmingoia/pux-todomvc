6231f206a46b074d093969a3a59be554f67605b1
Alex Mingoia <talk@alexmingoia.com>
2017-03-30 22:22:44 -0700

    Remove conficting dependency.

--- src/App/Effects.purs
+++ src/App/Effects.purs
@@ -2,7 +2,6 @@ module App.Effects where
 
 import DOM (DOM)
 import DOM.HTML.Types (HISTORY)
-import DOM.WebStorage (STORAGE)
 import Network.HTTP.Affjax (AJAX)
 
-type AppEffects fx = (storage :: STORAGE, ajax :: AJAX, dom :: DOM, history :: HISTORY | fx)
+type AppEffects fx = (ajax :: AJAX, dom :: DOM, history :: HISTORY | fx)
--- src/Client.purs
+++ src/Client.purs
@@ -1,44 +1,48 @@
 module Client where
 
 import App.Effects (AppEffects)
+import App.Events (Event(..), foldp)
 import App.Routes (match)
 import App.State (State, init)
-import App.Events (Event(..), foldp)
 import App.View.Layout (view)
-import Control.Bind ((=<<), bind)
+import Control.Bind (bind)
 import Control.Monad.Eff (Eff)
 import DOM.HTML (window)
-import DOM.WebStorage (getLocalStorage)
-import DOM.WebStorage.JSON (getItem, setItem)
-import Data.Argonaut (Json, decodeJson)
-import Data.Either (either)
+import DOM.HTML.Window (localStorage)
+import DOM.WebStorage.Storage (getItem, setItem)
+import Data.Argonaut (Json, decodeJson, encodeJson)
+import Data.Argonaut.Parser (jsonParser)
+import Data.Either (Either(..), either)
 import Data.Function (id, ($))
-import Data.Generic (class Generic)
-import Data.Maybe (maybe)
+import Data.Functor ((<$>))
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.Nullable (toMaybe)
+import Data.Show (show)
 import Data.Unit (Unit)
 import Pux (CoreEffects, start)
 import Pux.DOM.History (sampleURL)
 import Pux.Renderer.React (renderToDOM)
 import Signal (runSignal, (~>))
 
-data LocalStorageKey a = LocalStorageKey
-
-derive instance genericLocalStorageKey :: Generic (LocalStorageKey a)
-
 main :: ∀ fx. String -> State -> Eff (CoreEffects (AppEffects fx)) Unit
 main url state = do
+  win <- window
   -- | Fetch previous state from localStorage
-  localStorage <- getLocalStorage
-  stored_state <- getItem localStorage LocalStorageKey
+  storage <- localStorage win
+  stored_state_json <- getItem "pux:state" storage
+  let initialState = case (jsonParser <$> toMaybe stored_state_json) of
+        Nothing -> state
+        Just (Left _) -> state
+        Just (Right json) -> readState json
 
   -- | Create a signal of URL changes.
-  urlSignal <- sampleURL =<< window
+  urlSignal <- sampleURL win
 
   -- | Map a signal of URL changes to PageView actions.
   let routeSignal = urlSignal ~> \r -> PageView (match r)
 
   app <- start
-    { initialState: maybe state (\s -> readState s) stored_state
+    { initialState
     , view
     , foldp
     , inputs: [routeSignal] }
@@ -47,7 +51,7 @@ main url state = do
 
   -- | Persist state to localStorage
   runSignal $ app.state ~> \st ->
-    setItem localStorage LocalStorageKey st
+    setItem "pux:state" (show (encodeJson st)) storage
 
 readState :: Json -> State
 readState json = either (\_ -> init "/") id $ decodeJson json
